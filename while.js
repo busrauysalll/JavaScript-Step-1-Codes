c546cbe886b0cc4ccf87f316950e6f9f1c592b80
busrauysalll <bsryslbsrysl1995@gmail.com>
2021-04-06 23:11:53 +0300

    added new line

---
+++ while.js
@@ -0,0 +1,38 @@
+/*var sayi=10
+while(sayi>=0)
+{
+    console.log(sayi)
+    sayi--
+
+}
+*/
+/*var sayi=0
+while(sayi<=100){
+    if(sayi%2==0)
+    console.log(sayi)
+    
+    sayi++
+
+}
+*/
+/*
+//çift sayılar
+var sayi=0
+while(sayi<=100)
+{
+    console.log(sayi)
+    sayi+=2
+
+}
+*/
+
+/*
+//tek sayılar
+var sayi=1
+while(sayi<=100)
+{
+    console.log(sayi)
+    sayi+=2
+
+}
+*/
